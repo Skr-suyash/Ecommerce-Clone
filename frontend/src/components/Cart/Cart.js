e80793d144266220473e2f4409bdbaddcb78915f
Suyash Kumar <suyashkumar2005@gmail.com>
2020-12-22 23:23:48 +0530

    Added a checkout component and some fixes

--- frontend/src/components/Cart/Cart.js
+++ frontend/src/components/Cart/Cart.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import Subtotal from '../Subtotal/Subtotal';
 import './Cart.css';
 
 function Cart() {
   return (
-    <div>
-      <h1>I am Cart.</h1>
+    <div className="cart">
+      <div className="cart__left">
+        {/* <img
+          className="cart__left__image"
+          src="banner.jpg"
+          alt="banner"
+        /> */}
+        <h1 className="cart__hero">Your Shopping Cart</h1>
+      </div>
+      <div className="cart__right">
+        <Subtotal />
+      </div>
     </div>
   );
 }

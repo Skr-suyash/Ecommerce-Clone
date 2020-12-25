9ac68ff3e31cdbeedc0d8dd75bda0fc20bf3baa4
Suyash Kumar <suyashkumar2005@gmail.com>
2020-12-25 20:07:24 +0530

    Fixed some problems with auth system

--- backend/util/db/mongoose.js
+++ backend/util/db/mongoose.js
@@ -5,6 +5,8 @@ const DB_URL = 'mongodb://127.0.0.1:27017/ecommerce-clone';
 mongoose.connect(DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: true,
 })
   // eslint-disable-next-line no-console
   .then(console.log(`MongoDB Connected on ${DB_URL}`))
